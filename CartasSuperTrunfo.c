6e664bca0b60a284a62df2d57ee81b1f368a83c1
KayoChermont <kayochermont@icloud.com>
2025-02-25 00:09:48 +0000

    Adição de novas informações da carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,58 +10,26 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
 
-    char nomeA01[20];
-    int codigoA01
-    float populacaoA01
-    float areaA01
-    float pibA01
-    int ptA01
+    char nomeA01[20] = "novafriburgo";
+    float codigoA01 = 2525, populacaoA01 = 25.258, areaA01 = 258.258;
+    double pibA01 = 25.258258;
+    int ptA01 = 32;
 
-    char A02
-    char A03
-    char A04
+    printf ("Bem vindo ao jogo Super Trunfo - Países \nA seguir, insira as informações das cartas:\n\n");
+    //printf ("Nome da cidade: ");
+    //scanf ("%s", &nomeA01);
+    printf ("Código: ");
+    scanf ("%4f", &codigoA01);
+    printf ("População: ");
+    scanf ("%6f", &populacaoA01);
+    printf ("Área: ");
+    scanf ("%f", &areaA01);
+    printf ("PIB: ");
+    scanf ("%f", &pibA01);
+    printf ("Pontos turisticos: ");
+    scanf ("%d", &ptA01);
 
-    char B01
-    char B01
-    char B01
-    char B01
-
-    char C01
-    char C02
-    char C03
-    char C04
-
-    char D01
-    char D02
-    char D03
-    char D04
-    
-    char E01
-    char E02
-    char E03
-    char E04
-
-    char F01
-    char F02
-    char F03
-    char F04
-
-    char G01
-    char G02
-    char G03
-    char G04
-
-    char H01
-    char H02
-    char H03
-    char H04
-
-    printf ("Bem vindo ao jogo Super Trunfo - Países \n A seguir, insira as informações das cartas:");
-    printf ("Nome da cidade:\nCódigo:\nPopulação:\nÁrea:\nPIB:\nQuantidade de pobntos turísticos:\n");
-    scanf ("%s %n %f %f %f %n", &nomeA01, &codigoA01, &populacaoA01, &areaA01, &pibA01, &ptA01);
-    printf ("Cidade: %s\nCódigo;%n")
-
-
+    printf ("\n\nCarta adicionada: \n%s \n%f \n%f \n%f \n%f \n%d", nomeA01, codigoA01, populacaoA01, areaA01, pibA01, ptA01);
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
@@ -72,4 +40,5 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
+    
 }
