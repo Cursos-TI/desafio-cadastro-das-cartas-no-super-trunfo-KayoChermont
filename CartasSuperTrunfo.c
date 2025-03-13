dcf67c1915bb90222a67d5ddd34ace9182cf966e
KayoChermont <kayochermont@icloud.com>
2025-03-13 00:19:30 +0000

    adicionado a densidade e o pib per capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,6 +13,8 @@ int main() {
     float area01, area02;
     float pib01, pib02;
     int pontosturisticos01, pontosturisticos02;
+    float densidade01, densidade02;
+    float pibpercapita01, pibpercapita02;
 
     //Boas vindas e solicitação de cadastro de cartas
 
@@ -56,14 +58,20 @@ int main() {
     printf ("Pontos turisticos: ");
     scanf (" %d", &pontosturisticos02);
     
+     //definindo valor da densidade e do PIB per capita
+
+    densidade01 = (populacao01 / area01);
+    densidade02 = (populacao02 / area02);
+    pibpercapita01 = (pib01 / populacao01);
+    pibpercapita02 = (pib02 / populacao02);
+    
     //mostrando informaçoes das duas cartas
 
-    printf ("\n\nCarta 1: \n\nEstado: %c \nCidade: %s \nCódigo: %s \nPopulação: %d \nÁrea: %.2f km²\nPIB: %.3f \nPontos Turísticos: %d", 
-        estado01, cidade01, codigo01, populacao01, area01, pib01, pontosturisticos01);
+    printf ("\n\nCarta 1: \n\nEstado: %c \nCidade: %s \nCódigo: %s \nPopulação: %d \nÁrea: %.2f km²\nPIB: %.3f bilhões de reais \nPontos Turísticos: %d \nDensidade Populacional: %.2f hab/km² \nPIB per Capita: %.2f reais \n", 
+        estado01, cidade01, codigo01, populacao01, area01, pib01, pontosturisticos01, densidade01, pibpercapita01);
 
-    printf ("\n\nCarta 2: \n\nEstado: %c \nCidade: %s \nCódigo: %s \nPopulação: %d \nÁrea: %.2f km²\nPIB: %.3f \nPontos Turísticos: %d\n", 
-        estado02, cidade02, codigo02, populacao02, area02, pib02, pontosturisticos02);
-
+    printf ("\n\nCarta 2: \n\nEstado: %c \nCidade: %s \nCódigo: %s \nPopulação: %d \nÁrea: %.2f km²\nPIB: %.3f bilhões de reais \nPontos Turísticos: %d\nDensidade Populacional: %.2f hab/km² \nPIB per Capita: %.2f reais \n", 
+        estado02, cidade02, codigo02, populacao02, area02, pib02, pontosturisticos02, densidade02, pibpercapita02);
 
     return 0;
     
